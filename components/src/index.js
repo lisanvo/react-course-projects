28fd5b33a83dbbe69c00d64f048d4b4f5e9117c6
Lisa Vo <lisanvo@Lisas-MacBook-Pro.local>
2020-05-10 13:39:13 -0700

    Added components app

---
+++ components/src/index.js
@@ -0,0 +1,41 @@
+import ApprovalCard from './ApprovalCard';
+import CommentDetail from './CommentDetail';
+import faker from 'faker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                Are you sure you want to do this?
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    content="Nice blog post"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 2:00AM"  
+                    content="I like this subject" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Yesterday at 5:00PM" 
+                    content="I like the writing" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
